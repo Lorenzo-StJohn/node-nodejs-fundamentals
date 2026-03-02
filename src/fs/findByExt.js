086b6d5f5e46d6cb2035a3a53b230680ba587bb0
Lorenzo-StJohn <britishaccent@bk.ru>
2026-03-02 21:17:13 +0400

    feat: implement getting relative paths

--- src/fs/findByExt.js
+++ src/fs/findByExt.js
@@ -13,12 +13,72 @@ const findByExt = async () => {
   let ext;
   try {
     const { values } = parseArgs({ options });
-    ext = values.ext;
+    ext = values.ext.trim();
   } catch (err) {
     console.error(err);
     console.log(
       'P. S. There are should be either exactly one argument named ext with its value or no arguments at all.',
     );
+  }
+
+  const FOLDER_NAME = 'workspace';
+  const pathToThisFile = fileURLToPath(import.meta.url);
+  const pathToThisFolder = dirname(pathToThisFile);
+  const pathToRoot = join(pathToThisFolder, '..', '..');
+
+  const getFolderPath = async (pathToRoot, pathToThisFolder, folderName) => {
+    let isFolderInRoot;
+    let isFolderInThisFolder;
+    const pathToFolderInRoot = join(pathToRoot, folderName);
+    const pathToFolderInThisFolder = join(pathToThisFolder, folderName);
+    try {
+      await access(pathToFolderInRoot);
+      return pathToFolderInRoot;
+    } catch (err) {
+      isFolderInRoot = false;
+    }
+    try {
+      await access(pathToFolderInThisFolder);
+      return pathToFolderInThisFolder;
+    } catch (err) {
+      isFolderInThisFolder = false;
+    }
+    if (!isFolderInRoot && !isFolderInThisFolder) {
+      throw new Error('FS operation failed');
+    }
+  };
+
+  let pathToFolder;
+  try {
+    pathToFolder = await getFolderPath(
+      pathToRoot,
+      pathToThisFolder,
+      FOLDER_NAME,
+    );
+  } catch (err) {
+    console.error(err);
+    console.log(
+      'P. S. Folder named workspace should be either in project root folder or in src/fs folder.',
+    );
+    return;
+  }
+
+  const pathWithRecursion = join(pathToFolder, '**', `*.${ext}`);
+
+  const entries = [];
+
+  const getEntries = async (pathToFolder, pathWithRecursion) => {
+    for await (const entry of glob(pathWithRecursion)) {
+      const path = relative(pathToFolder, entry);
+      entries.push(path);
+    }
+  };
+
+  try {
+    await getEntries(pathToFolder, pathWithRecursion);
+  } catch (err) {
+    console.error(err);
+    return;
   }
 };
 
