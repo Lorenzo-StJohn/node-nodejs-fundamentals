e7ec74b75d149afe131b0cc3b138378629789031
Lorenzo-StJohn <britishaccent@bk.ru>
2026-03-02 20:58:56 +0400

    feat: implement reading ext argument from cli

--- src/fs/findByExt.js
+++ src/fs/findByExt.js
@@ -1,7 +1,25 @@
+import { parseArgs } from 'util';
+
 const findByExt = async () => {
   // Write your code here
   // Recursively find all files with specific extension
   // Parse --ext CLI argument (default: .txt)
+  const options = {
+    ext: {
+      type: 'string',
+      default: 'txt',
+    },
+  };
+  let ext;
+  try {
+    const { values } = parseArgs({ options });
+    ext = values.ext;
+  } catch (err) {
+    console.error(err);
+    console.log(
+      'P. S. There are should be either exactly one argument named ext with its value or no arguments at all.',
+    );
+  }
 };
 
 await findByExt();
