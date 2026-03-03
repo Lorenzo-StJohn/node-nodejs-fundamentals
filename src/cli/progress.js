27c0bf6f164fab691e620ea3e464ff343c6c5b0a
Lorenzo-StJohn <britishaccent@bk.ru>
2026-03-03 23:49:47 +0400

    feat: implement constructing parts of progress bar

--- src/cli/progress.js
+++ src/cli/progress.js
@@ -3,6 +3,23 @@ const progress = () => {
   // Simulate progress bar from 0% to 100% over ~5 seconds
   // Update in place using \r every 100ms
   // Format: [████████████████████          ] 67%
+
+  const constructProgressBarParts = (
+    startTime,
+    duration,
+    length,
+    color,
+    reset,
+  ) => {
+    const progressStatus = Math.min((Date.now() - startTime) / duration, 1);
+    const filledLength = Math.floor(progressStatus * length);
+    const emptyLength = length - filledLength;
+    const beginning = '[';
+    const filledPart = '█'.repeat(filledLength);
+    const emptyPart = ' '.repeat(emptyLength);
+    const ending = `] ${(100 * progressStatus).toFixed(0)}%`;
+    return `${beginning}${color}${filledPart}${reset}${emptyPart}${ending}`;
+  };
 };
 
 progress();
