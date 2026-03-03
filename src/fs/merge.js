8bc9b0b8e86f18cd5c3b08f3b4ffac8b09b7056a
Lorenzo-StJohn <britishaccent@bk.ru>
2026-03-03 15:29:51 +0400

    refactor: make function find parts folder instead of workpace

--- src/fs/merge.js
+++ src/fs/merge.js
@@ -6,15 +6,25 @@ const merge = async () => {
   // The workspace directory should be either in project folder or in src/fs
 
   const FOLDER_NAME = 'workspace';
+  const PARTS_FOLDER_NAME = 'parts';
   const pathToThisFile = fileURLToPath(import.meta.url);
   const pathToThisFolder = dirname(pathToThisFile);
   const pathToRoot = join(pathToThisFolder, '..', '..');
 
-  const getFolderPath = async (pathToRoot, pathToThisFolder, folderName) => {
+  const getFolderPath = async (
+    pathToRoot,
+    pathToThisFolder,
+    folderName,
+    partsName,
+  ) => {
     let isFolderInRoot;
     let isFolderInThisFolder;
-    const pathToFolderInRoot = join(pathToRoot, folderName);
-    const pathToFolderInThisFolder = join(pathToThisFolder, folderName);
+    const pathToFolderInRoot = join(pathToRoot, folderName, partsName);
+    const pathToFolderInThisFolder = join(
+      pathToThisFolder,
+      folderName,
+      partsName,
+    );
     try {
       await access(pathToFolderInRoot);
       return pathToFolderInRoot;
@@ -33,16 +43,19 @@ const merge = async () => {
   };
 
   let pathToFolder;
+  let pathToFolderWithParts;
   try {
-    pathToFolder = await getFolderPath(
+    pathToFolderWithParts = await getFolderPath(
       pathToRoot,
       pathToThisFolder,
       FOLDER_NAME,
+      PARTS_FOLDER_NAME,
     );
+    pathToFolder = dirname(pathToFolderWithParts);
   } catch (err) {
     console.error(err);
     console.log(
-      'P. S. Folder named workspace should be either in project root folder or in src/fs folder.',
+      'P. S. Folder named workspace with folder named parts in it should be either in project root folder or in src/fs folder.',
     );
     return;
   }
