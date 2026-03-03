2c0372ca181d988666ba50b85deb8921c51a2fa6
Lorenzo-StJohn <britishaccent@bk.ru>
2026-03-03 15:18:54 +0400

    feat: implement finding workspace folder for merge function

--- src/fs/merge.js
+++ src/fs/merge.js
@@ -1,8 +1,51 @@
+import { access } from 'fs/promises';
+import { join, dirname } from 'path';
+import { fileURLToPath } from 'url';
+
 const merge = async () => {
-  // Write your code here
-  // Default: read all .txt files from workspace/parts in alphabetical order
-  // Optional: support --files filename1,filename2,... to merge specific files in provided order
-  // Concatenate content and write to workspace/merged.txt
+  // The workspace directory should be either in project folder or in src/fs
+
+  const FOLDER_NAME = 'workspace';
+  const pathToThisFile = fileURLToPath(import.meta.url);
+  const pathToThisFolder = dirname(pathToThisFile);
+  const pathToRoot = join(pathToThisFolder, '..', '..');
+
+  const getFolderPath = async (pathToRoot, pathToThisFolder, folderName) => {
+    let isFolderInRoot;
+    let isFolderInThisFolder;
+    const pathToFolderInRoot = join(pathToRoot, folderName);
+    const pathToFolderInThisFolder = join(pathToThisFolder, folderName);
+    try {
+      await access(pathToFolderInRoot);
+      return pathToFolderInRoot;
+    } catch (err) {
+      isFolderInRoot = false;
+    }
+    try {
+      await access(pathToFolderInThisFolder);
+      return pathToFolderInThisFolder;
+    } catch (err) {
+      isFolderInThisFolder = false;
+    }
+    if (!isFolderInRoot && !isFolderInThisFolder) {
+      throw new Error('FS operation failed');
+    }
+  };
+
+  let pathToFolder;
+  try {
+    pathToFolder = await getFolderPath(
+      pathToRoot,
+      pathToThisFolder,
+      FOLDER_NAME,
+    );
+  } catch (err) {
+    console.error(err);
+    console.log(
+      'P. S. Folder named workspace should be either in project root folder or in src/fs folder.',
+    );
+    return;
+  }
 };
 
 await merge();
