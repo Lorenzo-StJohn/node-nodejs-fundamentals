63a31a08b6c26c6e9128be1ecb0e819b40ee8510
Lorenzo-StJohn <britishaccent@bk.ru>
2026-03-05 02:05:10 +0400

    feat: implement lineNumberTransform

--- src/streams/lineNumberer.js
+++ src/streams/lineNumberer.js
@@ -7,7 +7,34 @@ const lineNumberer = () => {
   // Use Transform Stream to prepend line numbers
   // Write to process.stdout
 
-  const lineNumberTransform = new Transform();
+  const lineNumberTransform = new Transform({
+    transform(chunk, encoding, callback) {
+      const END_LINE = /(\r?\n)/;
+      const inputString = chunk.toString('utf8');
+      const inputLines = inputString.split(END_LINE);
+      if (inputLines.length > 2 && inputLines.at(-2).match(END_LINE)) {
+        inputLines.pop();
+        inputLines.pop();
+      }
+      let outputString = '';
+      this.lineCount ??= 1;
+      for (const inputLine of inputLines) {
+        if (inputLine.match(END_LINE)) {
+          outputString += inputLine;
+        } else {
+          const linesSplitted = inputLine.split('\\n');
+          const outputArr = [];
+          for (const lineSplitted of linesSplitted) {
+            outputArr.push(`${this.lineCount++} | ${lineSplitted}`);
+          }
+          const outputLine = outputArr.join('\\n');
+          outputString += outputLine;
+        }
+      }
+      outputString += '\n';
+      callback(null, outputString);
+    },
+  });
 
   const handlePipeline = async (...items) => {
     try {
