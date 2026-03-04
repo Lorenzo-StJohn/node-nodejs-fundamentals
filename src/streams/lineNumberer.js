7b53dcc7a13279b4a47cff334a73047cb6cb7c70
Lorenzo-StJohn <britishaccent@bk.ru>
2026-03-05 00:34:10 +0400

    feat: create pipeline for lineNumber function

--- src/streams/lineNumberer.js
+++ src/streams/lineNumberer.js
@@ -1,8 +1,24 @@
+import { Transform } from 'stream';
+import { pipeline } from 'stream/promises';
+
 const lineNumberer = () => {
   // Write your code here
   // Read from process.stdin
   // Use Transform Stream to prepend line numbers
   // Write to process.stdout
+
+  const lineNumberTransform = new Transform();
+
+  const handlePipeline = async (...items) => {
+    try {
+      await pipeline(...items);
+    } catch (err) {
+      console.error(err);
+      return;
+    }
+  };
+
+  void handlePipeline(process.stdin, lineNumberTransform, process.stdout);
 };
 
 lineNumberer();
