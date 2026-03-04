3c29b11ae951f25f94e4c6f775fc9aa8d3d612a3
Lorenzo-StJohn <britishaccent@bk.ru>
2026-03-04 18:02:11 +0400

    feat: implement reading plugin arguments

--- src/modules/dynamic.js
+++ src/modules/dynamic.js
@@ -1,9 +1,29 @@
+import { parseArgs } from 'util';
+
 const dynamic = async () => {
   // Write your code here
   // Accept plugin name as CLI argument
   // Dynamically import plugin from plugins/ directory
   // Call run() function and print result
   // Handle missing plugin case
+
+  const options = {
+    plugin: {
+      type: 'string',
+      default: [],
+      multiple: true,
+    },
+  };
+
+  let plugins;
+  try {
+    const { values } = parseArgs({ options, strict: false });
+    plugins = values.plugin;
+  } catch (err) {
+    console.error(err);
+    console.log('P. S. Reading arguments failed.');
+    return;
+  }
 };
 
 await dynamic();
