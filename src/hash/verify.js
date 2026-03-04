70e3986c2e305836f60e46f360ca590b6875f107
Lorenzo-StJohn <britishaccent@bk.ru>
2026-03-04 21:13:19 +0400

    feat: implement hash calculation

--- src/hash/verify.js
+++ src/hash/verify.js
@@ -1,8 +1,20 @@
+import { createHash } from 'crypto';
+import { createReadStream } from 'fs';
+
 const verify = async () => {
   // Write your code here
   // Read checksums.json
   // Calculate SHA256 hash using Streams API
   // Print result: filename — OK/FAIL
+
+  const calculateHash = async (path, algorithm) => {
+    const hash = createHash(algorithm);
+    const stream = createReadStream(path);
+    for await (const chunk of stream) {
+      hash.update(chunk);
+    }
+    return hash.digest('hex');
+  };
 };
 
 await verify();
