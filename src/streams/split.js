c51460179d57a777efbcadcc3752ae87feff66cd
Lorenzo-StJohn <britishaccent@bk.ru>
2026-03-06 02:02:30 +0400

    feat: implement finding source.txt

--- src/streams/split.js
+++ src/streams/split.js
@@ -1,12 +1,54 @@
 import { parseArgs } from 'util';
+import { access } from 'fs/promises';
+import { join, dirname } from 'path';
+import { fileURLToPath } from 'url';
 
 const split = async () => {
-  // Write your code here
-  // Read source.txt using Readable Stream
-  // Split into chunk_1.txt, chunk_2.txt, etc.
-  // Each chunk max N lines (--lines CLI argument, default: 10)
+  // File source.txt should be either in project folder or in src/streams
 
   const DEFAULT_LINES = '10';
+  const FILE_NAME = 'source.txt';
+
+  const pathToThisFile = fileURLToPath(import.meta.url);
+  const pathToThisFolder = dirname(pathToThisFile);
+  const pathToRoot = join(pathToThisFolder, '..', '..');
+
+  const getFilePath = async (pathToRoot, pathToThisFolder, fileName) => {
+    let isFileInRoot;
+    let isFileInThisFolder;
+    const pathToFileInRoot = join(pathToRoot, fileName);
+    const pathToFileInThisFolder = join(pathToThisFolder, fileName);
+    try {
+      await access(pathToFileInRoot);
+      return pathToFileInRoot;
+    } catch (err) {
+      isFileInRoot = false;
+    }
+    try {
+      await access(pathToFileInThisFolder);
+      return pathToFileInThisFolder;
+    } catch (err) {
+      isFileInThisFolder = false;
+    }
+    if (!isFileInRoot && !isFileInThisFolder) {
+      throw new Error('File source.txt not found');
+    }
+  };
+
+  let pathToSourceFile;
+  try {
+    pathToSourceFile = await getFilePath(
+      pathToRoot,
+      pathToThisFolder,
+      FILE_NAME,
+    );
+  } catch (err) {
+    console.error(err);
+    console.log(
+      'P. S. File named source.txt should be either in project root folder or in src/streams folder.',
+    );
+    return;
+  }
 
   const options = {
     lines: {
