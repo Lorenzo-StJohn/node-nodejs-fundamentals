78fb3be0f8bb57a7eb22cb46b45fe9fe27c749d1
Lorenzo-StJohn <britishaccent@bk.ru>
2026-03-06 15:26:15 +0400

    feat: implement createMetadata function

--- src/zip/compressDir.js
+++ src/zip/compressDir.js
@@ -1,9 +1,21 @@
+import { stat } from 'fs/promises';
+
 const compressDir = async () => {
   // Write your code here
   // Read all files from workspace/toCompress/
   // Compress entire directory structure into archive.br
   // Save to workspace/compressed/
   // Use Streams API
+
+  const createMetadata = async (entry) => {
+    const entryStat = await stat(entry);
+    const entryType = entryStat.isFile ? 'file' : 'directory';
+    const entryObj = {
+      path: entry,
+      type: entryType,
+    };
+    return JSON.stringify(entryObj);
+  };
 };
 
 await compressDir();
