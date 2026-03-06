5fd84de00cb53c6d450c7a2d19eaa85713d49f0e
Lorenzo-StJohn <britishaccent@bk.ru>
2026-03-06 04:00:26 +0400

    feat: implement transformToOneLine

--- src/streams/split.js
+++ src/streams/split.js
@@ -2,12 +2,16 @@ import { parseArgs } from 'util';
 import { access } from 'fs/promises';
 import { join, dirname } from 'path';
 import { fileURLToPath } from 'url';
+import { createReadStream, createWriteStream } from 'fs';
+import { Transform } from 'stream';
+import { pipeline } from 'stream/promises';
 
 const split = async () => {
   // File source.txt should be either in project folder or in src/streams
 
   const DEFAULT_LINES = '10';
-  const FILE_NAME = 'source.txt';
+  const INPUT_FILE_NAME = 'source.txt';
+  const OUTPUT_FILE_NAME = 'chunk_';
 
   const pathToThisFile = fileURLToPath(import.meta.url);
   const pathToThisFolder = dirname(pathToThisFile);
@@ -40,7 +44,7 @@ const split = async () => {
     pathToSourceFile = await getFilePath(
       pathToRoot,
       pathToThisFolder,
-      FILE_NAME,
+      INPUT_FILE_NAME,
     );
   } catch (err) {
     console.error(err);
@@ -68,6 +72,40 @@ const split = async () => {
     console.log('P. S. Reading arguments failed.');
     return;
   }
+
+  const path = join(pathToThisFolder, `${OUTPUT_FILE_NAME}1.txt`);
+  const readStream = createReadStream(pathToSourceFile);
+  const writeStream = createWriteStream(path);
+
+  const transformToOneLine = new Transform({
+    transform(chunk, encoding, callback) {
+      const END_LINE = /\r?\n/;
+      const inputString = chunk.toString('utf8');
+      this.accumulatedString ??= '';
+      this.accumulatedString += inputString;
+      const stringSplitted = this.accumulatedString.split(END_LINE);
+      this.accumulatedString = stringSplitted.pop();
+      for (let i = 0; i < stringSplitted.length; ++i) {
+        this.push(stringSplitted[i] + '\n');
+      }
+      callback();
+    },
+
+    flush(callback) {
+      this.push(this.accumulatedString);
+      callback();
+    },
+  });
+
+  const handlePipeline = async (...items) => {
+    try {
+      await pipeline(...items);
+    } catch (err) {
+      console.error(err);
+      return;
+    }
+  };
+  void handlePipeline(readStream, transformToOneLine, writeStream);
 };
 
 await split();
