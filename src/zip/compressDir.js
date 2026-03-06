52faab98e76e665b0f4abccd63618dd4d9fdfbf9
Lorenzo-StJohn <britishaccent@bk.ru>
2026-03-06 16:18:51 +0400

    feat: implement finding toCompress folder

--- src/zip/compressDir.js
+++ src/zip/compressDir.js
@@ -1,15 +1,56 @@
 import { stat } from 'fs/promises';
 import { Transform } from 'stream';
 import { createBrotliCompress } from 'zlib';
+import { join, dirname } from 'path';
+import { fileURLToPath } from 'url';
 
 const compressDir = async () => {
-  // Write your code here
-  // Read all files from workspace/toCompress/
-  // Compress entire directory structure into archive.br
-  // Save to workspace/compressed/
-  // Use Streams API
+  // Folder workspace should be either in project root folder or in src/zip
+  // Folder toCompress should be inside workspace folder
 
   const COMPRESS_LEVEL = 4;
+  const FOLDER_PATHS = ['workspace', 'toCompress'];
+
+  const pathToThisFile = fileURLToPath(import.meta.url);
+  const pathToThisFolder = dirname(pathToThisFile);
+  const pathToRoot = join(pathToThisFolder, '..', '..');
+
+  const getFolderPath = async (pathToRoot, pathToThisFolder, folderPaths) => {
+    let isFolderInRoot;
+    let isFolderInThisFolder;
+    const pathToFolderInRoot = join(pathToRoot, ...folderPaths);
+    const pathToFolderInThisFolder = join(pathToThisFolder, ...folderPaths);
+    try {
+      await access(pathToFolderInRoot);
+      return pathToFolderInRoot;
+    } catch (err) {
+      isFolderInRoot = false;
+    }
+    try {
+      await access(pathToFolderInThisFolder);
+      return pathToFolderInThisFolder;
+    } catch (err) {
+      isFolderInThisFolder = false;
+    }
+    if (!isFolderInRoot && !isFolderInThisFolder) {
+      throw new Error('FS operation failed');
+    }
+  };
+
+  let pathToFolder;
+  try {
+    pathToFolder = await getFolderPath(
+      pathToRoot,
+      pathToThisFolder,
+      FOLDER_PATHS,
+    );
+  } catch (err) {
+    console.error(err);
+    console.log(
+      'P. S. Folder named workspace with folder named toCompress should be either in project root folder or in src/zip.',
+    );
+    return;
+  }
 
   const stringTransform = new Transform({
     transform(chunk, encoding, callback) {
