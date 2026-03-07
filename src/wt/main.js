946012b544d9efac30ba48572b8e7d8b2268e410
Lorenzo-StJohn <britishaccent@bk.ru>
2026-03-07 07:11:04 +0400

    feat: read data.json

--- src/wt/main.js
+++ src/wt/main.js
@@ -1,4 +1,7 @@
 import { cpus } from 'os';
+import { access, readFile } from 'fs/promises';
+import { join, dirname } from 'path';
+import { fileURLToPath } from 'url';
 
 const main = async () => {
   // Write your code here
@@ -9,7 +12,60 @@ const main = async () => {
   // Merge using k-way merge algorithm
   // Log final sorted array
 
+  // The data.json should be either in project folder or in src/wt
+
   const n = cpus().length;
+
+  const JSON_NAME = 'data.json';
+  const pathToThisFile = fileURLToPath(import.meta.url);
+  const pathToThisFolder = dirname(pathToThisFile);
+  const pathToRoot = join(pathToThisFolder, '..', '..');
+
+  const getFilePath = async (pathToRoot, pathToThisFolder, fileName) => {
+    let isFileInRoot;
+    let isFileInThisFolder;
+    const pathToFileInRoot = join(pathToRoot, fileName);
+    const pathToFileInThisFolder = join(pathToThisFolder, fileName);
+    try {
+      await access(pathToFileInRoot);
+      return pathToFileInRoot;
+    } catch (err) {
+      isFileInRoot = false;
+    }
+    try {
+      await access(pathToFileInThisFolder);
+      return pathToFileInThisFolder;
+    } catch (err) {
+      isFileInThisFolder = false;
+    }
+    if (!isFileInRoot && !isFileInThisFolder) {
+      throw new Error('FS operation failed');
+    }
+  };
+
+  let pathToJsonFile;
+  try {
+    pathToJsonFile = await getFilePath(pathToRoot, pathToThisFolder, JSON_NAME);
+  } catch (err) {
+    console.error(err);
+    console.log(
+      'P. S. File named data.json should be either in project root folder or in src/wt folder.',
+    );
+    return;
+  }
+
+  const readJsonFile = async (pathToJsonFile) => {
+    const data = await readFile(pathToJsonFile, { encoding: 'utf8' });
+    return JSON.parse(data);
+  };
+
+  let array;
+  try {
+    array = await readJsonFile(pathToJsonFile);
+  } catch (err) {
+    console.error(err);
+    return;
+  }
 };
 
 await main();
